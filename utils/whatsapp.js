4f818244e6166868070d9b0d022e757464abae93
aeroimports <aeroimports2016@gmail.com>
2025-03-18 13:50:13 -0300

    sem base 64

--- utils/whatsapp.js
+++ utils/whatsapp.js
@@ -100,7 +100,7 @@ const sender = async (url, payload) => {
            return { success: false, error: "Tipo de mensagem não suportado" };
        }
    
-       console.log(payload.media.base64);
+  
    
      /*  if (messageData.success) {
          const {
